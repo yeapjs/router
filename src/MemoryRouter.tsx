4a8eb1357267bced92fa29999ab6c968d19d422f
hanako-eo <enuxura@gmail.com>
2022-09-11 22:57:57 +0200

    feat: MemoryRouter

---
+++ src/MemoryRouter.tsx
@@ -0,0 +1,42 @@
+import { createReactor } from "yeap/app"
+import { RouterContext } from "./context"
+
+interface MemoryRouterProps {
+  initialLocation?: string
+}
+
+export function MemoryRouter(
+  { initialLocation = "/" }: MemoryRouterProps,
+  children: any
+) {
+  const location = createReactor(initialLocation)
+  let history = [initialLocation]
+  let i = 0
+
+  return (
+    <RouterContext.Provider
+      value={{
+        location,
+        push(newLocation) {
+          location(newLocation)
+          history = [...history.slice(0, i + 1), newLocation]
+          i++
+        },
+        replace(newLocation) {
+          location(newLocation)
+          history[i] = newLocation
+        },
+        back() {
+          if (i <= 0) return
+          location(history[--i])
+        },
+        forward() {
+          if (i + 1 >= history.length) return
+          location(history[++i])
+        },
+      }}
+    >
+      {children}
+    </RouterContext.Provider>
+  )
+}
