4aeba937420f20b166bbccc5267fadd78f3d6962
hanako-eo <enuxura@gmail.com>
2022-09-11 22:57:39 +0200

    feat: forward and replace history

--- src/Link.tsx
+++ src/Link.tsx
@@ -5,16 +5,17 @@ import { RouterContext } from "./context"
 
 interface LinkProps extends JSX.ReactivableHTMLAttributes<HTMLAnchorElement> {
   back?: boolean
+  forward?: boolean
   replace?: boolean
   to?: string
   ref: Reactor<HTMLAnchorElement>
 }
 
 export function Link(
-  { back, replace = false, to = "/", ref, ...rest }: LinkProps,
+  { back, forward, replace = false, to = "/", ref, ...rest }: LinkProps,
   children: any
 ) {
-  const { location, navigateTo } = useContext(RouterContext)!
+  const history = useContext(RouterContext)!
 
   function handleClick(e: any) {
     if (rest["onClick:prevent"]) {
@@ -23,7 +24,8 @@ export function Link(
       else rest["onClick:prevent"][0](rest["onClick:prevent"].slice(1))
     }
     if (back) history.back()
-    else navigateTo(to)
+    else if (forward) history.forward()
+    else history[replace ? "replace" : "push"](to)
   }
 
   return (
@@ -32,7 +34,7 @@ export function Link(
       ref={ref}
       href={to}
       onClick:prevent={handleClick}
-      data-active={location.compute((v) => v === to)}
+      data-active={history.location.compute((v) => v === to)}
     >
       {children}
     </a>
